319e73848e857412b5543531c8b17e8a9677fc00
SusanLaime <susanlaimel1@upb.edu>
2025-07-13 15:01:04 -0400

    Implements carrousel for contributors

--- eng/index.php
+++ eng/index.php
@@ -83,7 +83,7 @@
                     </ul> 
                 </div>
                 <!-- PARTE SUSAN FIN -->
-                 <div class="container-fluid px-0">
+                 <!--<div class="container-fluid px-0">
                     <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
                         <div class="carousel-inner">
                             <div class="carousel-item active">
@@ -169,7 +169,7 @@
                         <li>Conference_Schedule_and_Important_Dates</li>
                     </ul> 
                 </div> -->
-                <div class="container-fluid py-5 bg-body-secondary">
+                <!--<div class="container-fluid py-5 bg-body-secondary">
                     <div class="row">
                         <div class="col-sm-12 text-sm-left">
                             <h2 class="display-5 fw-bold">Conference Schedule and Important Dates: </h2>               <p>Stay informed about key milestones and deadlines throughout the conference with our comprehensive timeline, ensuring you don't miss any important stages.</p>
@@ -486,8 +486,10 @@
                     </div>
                 </div>           
             </section> -->
+
+       
     <!-- Coordinators section -->
-    <!-- <section id="coordinators">
+    <section id="coordinators">
         <div class="container-fluid align-content-start py-0">
             <ul class="breadcrumb">
                 <li><a href="index.php">Home</a></li>
@@ -597,11 +599,11 @@
                                             <h6 class="card-text">Agatha da Silva Ovando<br>Technical Program Co-Chair</h6>
                                         </div>
                                         <div class="card-footer mt-auto d-flex flex-column">
-                                            <!--    <form action ="resume.php" method="POST">
+                                                <form action ="resume.php" method="POST">
                                                     <input type ="hidden" name="name" value="dr_da_silva">
                                                     <input type ="hidden" name="position" value="Technical Program Co-Chair">
                                                     <button class="border-white btn btn-primary btn-sm" type="submit">View CV</button> </form> -->
-                                            <!-- <p class="mb-0"> <a href="../../cv_congresses_en/dr_da_silva.pdf" class="border-white btn btn-primary btn-sm" role="button">View CV</a></p>           
+                                            <p class="mb-0"> <a href="../../cv_congresses_en/dr_da_silva.pdf" class="border-white btn btn-primary btn-sm" role="button">View CV</a></p>           
                                         </div>
                                     </div>
                                 </div>
@@ -653,12 +655,12 @@
                                         <div class="card-body text-center d-flex flex-column">
                                             <h6 class="card-text">Rafael R. Rentería Ramos<br>Workshops Chair</h6>
                                         </div>
-                                        <div class="card-footer mt-auto d-flex flex-column"> -->
-                                            <!-- <form action ="resume.php" method="POST">
+                                        <div class="card-footer mt-auto d-flex flex-column"> 
+                                            <form action ="resume.php" method="POST">
                                                 <input type ="hidden" name="name" value="dr_renteria">
                                                 <input type ="hidden" name="position" value="Workshops Chair">
-                                                <button class="border-white btn btn-primary btn-sm" type="submit">View CV</button> </form> -->
-                                            <!-- <p class="mb-0"> <a href="../../cv_congresses_en/dr_renteria.pdf" class="border-white btn btn-primary btn-sm" role="button">View CV</a></p>    
+                                                <button class="border-white btn btn-primary btn-sm" type="submit">View CV</button> </form> 
+                                            <p class="mb-0"> <a href="../../cv_congresses_en/dr_renteria.pdf" class="border-white btn btn-primary btn-sm" role="button">View CV</a></p>    
                                         </div>
                                     </div> 
 
@@ -711,7 +713,228 @@
                 </div>
             </div> 
         </div>
-    </section>  -->
+    </section> 
+
+    <section> 
+        <!--V3-->
+        <div id="carouselExampleCaptions" class="carousel slide">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            </div>
+            <div class="carousel-inner">
+                
+                <div class="carousel-item active">
+                    <div class="row h-100 justify-content-center align-items-center">
+                        <div class="col image-hover">
+                         <!--Changing this attribute: class="d-block w-100"  for class:"img-fluid"-->
+                             <img src="../images/logos_cooperation/logo_sbc.gif"
+                                class="img-fluid"
+                                alt="Logo SBC">
+                            <div class="overlay-content">
+                                <h5>Some Text</h5>
+                                <button class="btn btn-light">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="col image-hover">
+                         <!--Changing this attribute: class="d-block w-100"  for class:"img-fluid"-->
+                             <img src="../images/logos_cooperation/logo_sbc.gif"
+                                class="img-fluid"
+                                alt="Logo SBC">
+                            <div class="overlay-content">
+                                <h5>Some Text</h5>
+                                <button class="btn btn-light">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="col image-hover">
+                         <!--Changing this attribute: class="d-block w-100"  for class:"img-fluid"-->
+                             <img src="../images/logos_cooperation/logo_sbc.gif"
+                                class="img-fluid"
+                                alt="Logo SBC">
+                            <div class="overlay-content">
+                                <h5>Some Text</h5>
+                                <button class="btn btn-light">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="col image-hover">
+                         <!--Changing this attribute: class="d-block w-100"  for class:"img-fluid"-->
+                             <img src="../images/logos_cooperation/logo_sbc.gif"
+                                class="img-fluid"
+                                alt="Logo SBC">
+                            <div class="overlay-content">
+                                <h5>Some Text</h5>
+                                <button class="btn btn-light">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                    </div>
+                </div>
+   
+                <div class="carousel-item">
+                    <!--<div class="row align-items-start">-->
+                    <div class="row h-100 justify-content-center align-items-center">
+                        <div class="col image-hover">
+                         <!--Changing this attribute: class="d-block w-100"  for class:"img-fluid"-->
+                             <img src="../images/logos_cooperation/logo_sbc.gif"
+                                class="img-fluid"
+                                alt="Logo SBC">
+                            <div class="overlay-content">
+                                <h5>Some Text</h5>
+                                <button class="btn btn-light">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="col image-hover">
+                         <!--Changing this attribute: class="d-block w-100"  for class:"img-fluid"-->
+                             <img src="../images/logos_cooperation/logo_sbc.gif"
+                                class="img-fluid"
+                                alt="Logo SBC">
+                            <div class="overlay-content">
+                                <h5>Some Text</h5>
+                                <button class="btn btn-light">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="col image-hover">
+                         <!--Changing this attribute: class="d-block w-100"  for class:"img-fluid"-->
+                             <img src="../images/logos_cooperation/logo_sbc.gif"
+                                class="img-fluid"
+                                alt="Logo SBC">
+                            <div class="overlay-content">
+                                <h5>Some Text</h5>
+                                <button class="btn btn-light">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="col image-hover">
+                         <!--Changing this attribute: class="d-block w-100"  for class:"img-fluid"-->
+                             <img src="../images/logos_cooperation/logo_sbc.gif"
+                                class="img-fluid"
+                                alt="Logo SBC">
+                            <div class="overlay-content">
+                                <h5>Some Text</h5>
+                                <button class="btn btn-light">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="carousel-item">
+                    <div class="row h-100 justify-content-center align-items-center">
+                        <div class="col image-hover">
+                         <!--Changing this attribute: class="d-block w-100"  for class:"img-fluid"-->
+                             <img src="../images/logos_cooperation/logo_sbc.gif"
+                                class="img-fluid"
+                                alt="Logo SBC">
+                            <div class="overlay-content">
+                                <h5>Some Text</h5>
+                                <button class="btn btn-light">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="col image-hover">
+                         <!--Changing this attribute: class="d-block w-100"  for class:"img-fluid"-->
+                             <img src="../images/logos_cooperation/logo_sbc.gif"
+                                class="img-fluid"
+                                alt="Logo SBC">
+                            <div class="overlay-content">
+                                <h5>Some Text</h5>
+                                <button class="btn btn-light">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="col image-hover">
+                         <!--Changing this attribute: class="d-block w-100"  for class:"img-fluid"-->
+                             <img src="../images/logos_cooperation/logo_sbc.gif"
+                                class="img-fluid"
+                                alt="Logo SBC">
+                            <div class="overlay-content">
+                                <h5>Some Text</h5>
+                                <button class="btn btn-light">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="col image-hover">
+                         <!--Changing this attribute: class="d-block w-100"  for class:"img-fluid"-->
+                             <img src="../images/logos_cooperation/logo_sbc.gif"
+                                class="img-fluid"
+                                alt="Logo SBC">
+                            <div class="overlay-content">
+                                <h5>Some Text</h5>
+                                <button class="btn btn-light">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="carousel-item">
+                    <div class="row h-100 justify-content-center align-items-center">
+                        <div class="col image-hover">
+                         <!--Changing this attribute: class="d-block w-100"  for class:"img-fluid"-->
+                             <img src="../images/logos_cooperation/logo_sbc.gif"
+                                class="img-fluid"
+                                alt="Logo SBC">
+                            <div class="overlay-content">
+                                <h5>Some Text</h5>
+                                <button class="btn btn-light">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="col image-hover">
+                         <!--Changing this attribute: class="d-block w-100"  for class:"img-fluid"-->
+                             <img src="../images/logos_cooperation/logo_sbc.gif"
+                                class="img-fluid"
+                                alt="Logo SBC">
+                            <div class="overlay-content">
+                                <h5>Some Text</h5>
+                                <button class="btn btn-light">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="col image-hover">
+                         <!--Changing this attribute: class="d-block w-100"  for class:"img-fluid"-->
+                             <img src="../images/logos_cooperation/logo_sbc.gif"
+                                class="img-fluid"
+                                alt="Logo SBC">
+                            <div class="overlay-content">
+                                <h5>Some Text</h5>
+                                <button class="btn btn-light">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="col image-hover">
+                         <!--Changing this attribute: class="d-block w-100"  for class:"img-fluid"-->
+                             <img src="../images/logos_cooperation/logo_sbc.gif"
+                                class="img-fluid"
+                                alt="Logo SBC">
+                            <div class="overlay-content">
+                                <h5>Some Text</h5>
+                                <button class="btn btn-light">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </section>  
+
     <!-- Venue section -->
     <!-- <section id="venue">
         <div class="container-fluid align-content-start py-0">
