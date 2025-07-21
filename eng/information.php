f20dbc15554e2827640c059162c90df5bc0fba27
sebascatala <sebas.catala.mir@gmail.com>
2025-07-21 03:32:40 -0400

    sidebar con su java

--- eng/information.php
+++ eng/information.php
@@ -228,6 +228,7 @@
                         </div>
                     </div>
                 </div>
+                
         </section>
 
         <!-- Determine your manuscript type -->
@@ -871,9 +872,15 @@
             <!-- footer  -->
             <?php include "../includesHtml/final.html" ?> 
         </div>
-        <script src ="../js/readmorereadless.js"></script>
-        <script src ="../js/backTop.js"></script>
+        
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-    </body>
+ 
+
+        <script src ="../js/readmorereadless.js"></script>
+        <script src ="../js/backTop.js"></script>
+
+        <script src="js/sidebar.js"></script>
+
+   </body>
 </html>                      
