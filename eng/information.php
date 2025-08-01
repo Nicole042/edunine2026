5258b9b7f7ae192751703f7c0a6cda72e0d48c8f
sebascatala <sebas.catala.mir@gmail.com>
2025-07-31 23:36:38 -0400

    scrollbar en vertical

--- eng/information.php
+++ eng/information.php
@@ -925,11 +925,11 @@
             </div>
 
             <!-- CONTROLES -->
-            <button class="carousel-control-prev" type="button" data-bs-target="#contentCarousel" data-bs-slide="prev">
+            <button class="carousel-control-prev" type="button" data-bs-target="#secondCarousel" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#contentCarousel" data-bs-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="#secondCarousel" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
