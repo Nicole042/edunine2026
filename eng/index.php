3be65c1eb911d22b15f9ff604239cc81cbc11c76
SusanLaime <susanlaimel1@upb.edu>
2025-08-12 23:11:24 -0400

    Bootstrap for navigation added

--- eng/index.php
+++ eng/index.php
@@ -23,6 +23,9 @@
 
         <link href="../css/modal.css" rel="stylesheet">
 
+        <!-- Bootstrap for navigation -->
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 
         <!-- Custom styles for this template -->
         <script src="https://kit.fontawesome.com/5dc88a8201.js" crossorigin="anonymous"></script>
